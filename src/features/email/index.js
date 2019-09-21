68653197cdee529a1dc12ce1209fc02d49feec67
dimazhuravlev <dmitryzhur@gmail.com>
2019-09-21 23:48:01 +0300

    change email link

--- src/features/email/index.js
+++ src/features/email/index.js
@@ -3,7 +3,7 @@ import styles from "./email.module.css";
 
 const Email = () => (
   <div className={styles.email}>
-    <a className={styles.link} href="mailto: udarnik35@yandex.ru">
+    <a className={styles.link} href="mailto: instrum35@mail.ru">
       <p>INSTRUM35</p>
       <p className={styles.leftspace}>@MAIL</p>
       <p>.RU</p>
